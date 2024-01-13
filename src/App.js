5e1b880244db7886325942320ee4b3cd12b379e1
unknown <lateefahbaba123@gmail.com>
2024-01-13 16:44:39 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+import Calculater from "./calcular.js";
+
+
+const App = ()=>{
+
+   return <div className="main">
+    
+
+  <Calculater/>
+
+
+   </div>
+   
+
+  }
+
+  export default App;
